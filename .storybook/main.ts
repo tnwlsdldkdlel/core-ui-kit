579a6fa1f434d7806422673481a80f31e2299978
tnwlsdldkdlel <tnwls2dkdlel@naver.com>
2025-12-12 12:01:01 +0900

    feat: Storybook 10.1.7로 업데이트
    - storybook 패키지를 8.1.11에서 10.1.7로 업데이트
    - @storybook/react-vite@10.1.7 설치
    - Storybook 8 addon 패키지들 설치 (호환성 유지)
    - .storybook/main.ts에서 addon 경로를 'storybook/addon-*' 형식으로 변경
    - Storybook 빌드 및 실행 테스트 완료
    - Vite 7 호환성 확보를 위한 준비 완료

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,10 @@ import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
+    'storybook/addon-links',
+    'storybook/addon-essentials',
+    'storybook/addon-interactions',
+    'storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/react-vite',
